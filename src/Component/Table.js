48f6af93508f9e87e5108f171c7b3da071de41a2
Francie-design <67443206+Francie-design@users.noreply.github.com>
2020-08-13 13:49:40 +0000

    Create Table.js

---
+++ src/Component/Table.js
@@ -0,0 +1,114 @@
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+class Table extends Component {
+    render(){
+        return( 
+            <div className=" col-md-8">
+  <div id="card">
+    <div className="card-body">
+                                                    
+<table className="table table-hover mb-5 ">
+    <tr>
+      <th><b>COUNTRY</b></th>
+      <th><b>TOTAL CASES</b></th>
+      <th><b>RECOVERED</b></th>
+      <th><b>ACTIVE CASES</b></th>
+      <th><b>TOTAL DEALTH</b></th>
+    </tr>
+  
+    <tr>
+      <td>Ghana</td>
+      <td>1,200</td>
+      <td>1,200</td>
+      <td>1,200</td>
+      <td>1,200</td>
+    </tr>
+      
+    
+    <tr class="myn">
+        <td>Ghana</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+      </tr>
+      
+    <tr>
+        <td>Ghana</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+      </tr>
+      
+    <tr>
+        <td>Ghana</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+      </tr>
+      
+    <tr>
+        <td>Ghana</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+      </tr>
+      
+    <tr>
+        <td>Ghana</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+      </tr>
+      
+    <tr>
+        <td>Ghana</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+      </tr>
+      
+    <tr>
+        <td>Ghana</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+      </tr>
+      
+    <tr>
+        <td>Ghana</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+      </tr>
+      
+    <tr>
+        <td>Ghana</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+        <td>1,200</td>
+      </tr>
+               </table>               
+            </div>
+            </div>
+            </div>
+            
+      
+          
+            
+        )
+    }
+    
+}
+export default Table
